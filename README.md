4d6b02504ad40763bd8d6d647df3fb9c8ca1bfb1
Peter Williams <40656404@live.napier.ac.uk>
2023-02-06 13:01:09 +1100

    initial feature
    Signed-off-by: Peter Williams <40656404@live.napier.ac.uk>

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Software Engineering Methods - Group 2 Assessment
 
+- Developer Build Status ![GitHub Workflow Status (with branch)](https://img.shields.io/github/actions/workflow/status/MelissaAstbury/SEMPopulationInformation/main.yml?branch=develop)
+- Master Build Status ![GitHub Workflow Status (with branch)](https://img.shields.io/github/actions/workflow/status/MelissaAstbury/SEMPopulationInformation/main.yml?branch=master)
 - License ![LICENSE](https://img.shields.io/github/license/MelissaAstbury/SEMPopulationInformation.svg?style=flat-square)
 - Release [![Releases](https://img.shields.io/github/release/MelissaAstbury/SEMPopulationInformation.svg?style=flat-square)](https://github.com/MelissaAstbury/SEMPopulationInformation/releases)
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,149 @@
 package com.napier.sem;
 
-public class App {
-    public static void main(String[] args) {
+import java.sql.*;
+import java.util.ArrayList;
+public class App
+{
+    public static void main(String[] args)
+    {
         System.out.println("Let's go! attempt 1");
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Extract employee salary information
+        ArrayList<Country> countries = a.getCountries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(countries.size());
+
+        // Display
+        a.printCountries(countries);
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * Gets all the countries
+     * @return A list of all countries.
+     */
+    public ArrayList<Country> getCountries()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Population "
+                            + "FROM country "
+                            + "ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract country information
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (rset.next())
+            {
+                Country country = new Country();
+                country.Code = rset.getString("Code");
+                country.Name = rset.getString("Name");
+                country.Continent = rset.getString("Continent");
+                country.Population = rset.getInt("Population");
+                countries.add(country);
+            }
+            return countries;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of countries.
+     * @param countries The list of countries to print.
+     */
+    public void printCountries(ArrayList<Country> countries)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-30s %-30s %-20s", "Code", "Name", "Continent", "Population"));
+        // Loop over all countries in the list
+        for (Country country : countries)
+        {
+            String country_string =
+                    String.format("%-10s %-30s %-30s %-20s",
+                            country.Code, country.Name, country.Continent, country.Population);
+            System.out.println(country_string);
+        }
     }
 }
