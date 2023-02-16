252bbef2b557d479e18a58a5dc6fbb2006def263
Ashley Wilson <40521598@live.napier.ac.uk>
2023-02-16 15:09:26 +0000

    Ashley - Cities for District, 1st commit
    Signed-off-by: Ashley <98708199+Wils100@users.noreply.github.com>

--- src/main/java/com/napier/sem/CityReporting.java
+++ src/main/java/com/napier/sem/CityReporting.java
@@ -23,6 +23,13 @@ public class CityReporting {
         ArrayList<City> cities02 = new ArrayList<City>();
         cities02 = getCitiesForContinentByPopulation("Asia");
         printCities(cities02);
+
+        // Report 03 - Cites for district by population
+        System.out.println("Report 03 - Cities for district by population");
+        System.out.println("Parameters: District = California");
+        ArrayList<City> cities03 = new ArrayList<City>();
+        cities03 = getCitiesForDistrictByPopulation("California");
+        printCities(cities03);
     }
 
     public ArrayList<City> getCitiesByPopulation()
@@ -75,6 +82,45 @@ public class CityReporting {
                             + "FROM city, country "
                             + "WHERE city.CountryCode = country.Code "
                             + "AND country.Continent = '" + continent + "'"
+                            + "ORDER BY city.Population DESC";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract country information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City city = new City();
+                Country country = new Country();
+                city.Country = new Country();
+                city.Name = rset.getString("Name");
+                city.District = rset.getString("District");
+                city.Population = rset.getInt("Population");
+                city.Country.Name = rset.getString("Country");
+                cities.add(city);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    public ArrayList<City> getCitiesForDistrictByPopulation(String district)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.Name, country.Name Country, city.District, city.Population "
+                            + "FROM city, country "
+                            + "WHERE city.CountryCode = country.Code "
+                            + "AND city.District = '" + district + "'"
                             + "ORDER BY city.Population DESC";
 
             // Execute SQL statement
